5b9ea2c997db38de5a3522c7cb99255d66fd684c
cdgat_000 <cdgat_000@coles-pc>
2017-08-04 03:09:05 -0400

    Replaced manually added files with project added through egit

---
+++ Scouting Database/src/TBATeamParser.java
@@ -0,0 +1,15 @@
+import main.*;
+
+public class TBATeamParser {
+	
+	private int team_number;
+
+	public TBATeamParser(int teamnumber) {
+		// TODO Auto-generated constructor stub
+		team_number = teamnumber;
+	}
+	
+	public String getTeamJSON() {
+		
+	}
+}
