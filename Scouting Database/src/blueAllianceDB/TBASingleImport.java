8539697ef37b651ed4017dcb2bcf6451df667c26
cdmcmullen55 <bd541331@ahschool.com>
2017-09-01 11:52:28 -0400

    TBASingleImport v 1.0

--- Scouting Database/src/blueAllianceDB/TBASingleImport.java
+++ Scouting Database/src/blueAllianceDB/TBASingleImport.java
@@ -1,10 +1,60 @@
 package blueAllianceDB;
 
+import java.sql.*;
+import localConstantStorage.ConnectionConstants;
+
 public class TBASingleImport {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private TBATeam team;
+	private Connection conn;
+	
+	public TBASingleImport(int number) {
+		team = new TBATeam(number);
 	}
-
+	
+	public boolean openConnection(){
+		try
+		{
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+			String url = ConnectionConstants.TBA_DB_CONNECTION_STRING;
+			conn = DriverManager.getConnection(url, ConnectionConstants.USER_ID, ConnectionConstants.PASS);
+			return true;
+		}
+		catch (Exception e)
+		{
+			System.err.println("Exception thrown:"); 
+      			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	public boolean insertTeam(){
+		try
+		{
+			Statement statement = conn.createStatement();
+			statement.execute("INSERT INTO Blue_Alliance_Data " + "VALUES (\'" + team.getTeamKey() + "\', \'" + 
+					  team.getTeamName() + "\', " + team.getTeamWins() + ", " + team.getTeamLosses() + ", " + 
+					  team.getTeamTies() + ", " + team.getOPR + ", " + team.getRookieYear() + ");");
+			return true;
+		}
+		catch (Exception e)
+		{
+			System.err.println("Exception thrown:");
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	public boolean runImport(){
+		if(!openConnection()){
+			System.err.println("There was a connection error");
+			return false;
+		}
+		else if(!insertTeam()){
+			System.err.println("There was a SQL query error");
+			return false;
+		}
+		else
+			return true;
+	}
 }
