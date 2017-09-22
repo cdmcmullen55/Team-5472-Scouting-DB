fb9bdc499e08d235d7cfb2067f578c5f77e25549
cdmcmullen55 <bd541331@ahschool.com>
2017-09-22 16:25:53 -0400

    Update Team_Match.java

--- Scouting Database/src/Team_Matches/Team_Match.java
+++ Scouting Database/src/Team_Matches/Team_Match.java
@@ -1,1 +1,48 @@
 package teamMatches;
+
+public class Team_Matches{
+
+  private String t_m_key;
+  private String comp_key;
+  private String team_key;
+  private int match_num;
+  private boolean baseline;
+  private boolean auto_none;
+  private boolean auto_break;
+  private int auto_gear_cent;
+  private int auto_gear_boil;
+  private int auto_gear_load;
+  private int auto_grnd_gear;
+  private int auto_load_gear;
+  private int auto_g_c_att;
+  private int auto_g_b_att;
+  private int auto_g_l_att;
+  private int auto_gear_drop;
+  private boolean auto_fuel_hopp;
+  private boolean auto_fuel_load;
+  private boolean auto_fuel_grnd;
+  private boolean auto_boil_high;
+  private boolean auto_boi_key;
+  private boolean auto_boi_cyc;
+  private boolean tele_none;
+  private boolean tele_break;
+  private int tele_gear_drop;
+  private int tele_gear_cent;
+  private int tele_gear_boil;
+  private int tele_gear_load;
+  private int tele_grnd_pckp;
+  private int tele_load_pckp;
+  private int tele_full_high;
+  private int tele_full_low;
+  private int tele_half_high;
+  private int tele_half_low;
+  private boolean takeoff_att;
+  private boolean tkoff_succ;
+  private int tkoff_speed;
+  private int dfnse_rate;
+  rivate boolean win;
+  private boolean rotors;
+  private boolean pascals;
+  private String comments;
+  
+}
